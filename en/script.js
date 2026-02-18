1c8d92b89014848b6032654ea882dc5bf5fec77a
gvzang <gevzangulo@gmail.com>
2026-02-17 21:34:09 -0600

    Create script.js

---
+++ en/script.js
@@ -0,0 +1,10 @@
+function filterProjects(category) {
+    const projects = document.querySelectorAll('.project-box');
+    projects.forEach(project => {
+        if (category === 'all' || project.classList.contains(category)) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    });
+}
